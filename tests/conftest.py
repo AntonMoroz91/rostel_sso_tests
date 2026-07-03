a38977c8a719ad143eff66ca7556069bee4acd4b
AntonMoroz91 <toni.moroz.00@mail.ru>
2026-07-03 15:57:03 +0300

    добавил флаги для стабильной работы Chrome в headless на CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,10 +41,15 @@ def driver(request):
 
     # ---- НОВОЕ: поддержка headless (для GitHub Actions) ----
     if request.config.getoption("--headless"):
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-setuid-sandbox")
 
     # Создаём драйвер
     driver = webdriver.Chrome(options=chrome_options)
